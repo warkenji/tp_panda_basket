bfdb83007597609ca4270ff8017be279dae05a2a
warkenji <warren.gsprd@gmail.com>
2018-11-18 16:42:36 -0400

    Ajout d'un panier

---
+++ panier.py
@@ -0,0 +1,13 @@
+from element_modele import ElementModele
+
+
+class Panier(ElementModele):
+    def __init__(self, app):
+        ElementModele.__init__(self, app, "panier", "models/basketball_hoop/basketball_hoop.obj", "models/basketball_hoop/basketball_hoop_diffuse_noAO.jpg")
+
+        # Modification de la rotation du panier de basket
+        self.set_rotation(90, 90, 0)
+
+        taille_panier = self.get_taille()
+        # Modification de la taille du panier de bakset
+        self.set_taille(1.8, 1.8 * taille_panier.z / taille_panier.x, 2.9 + 1.05)
